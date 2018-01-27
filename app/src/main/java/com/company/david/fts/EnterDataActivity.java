5254d27338f6490f35eb8f67f2a175c05d31a457
David Azevedo <davidazevedo1996@hotmail.com>
2018-01-27 18:21:51 +0000

    Working on new data submission | Error on asynctask

--- app/src/main/java/com/company/david/fts/EnterDataActivity.java
+++ app/src/main/java/com/company/david/fts/EnterDataActivity.java
@@ -1,9 +1,11 @@
 package com.company.david.fts;
 
 import android.icu.lang.UCharacterEnums;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,5 +26,38 @@ public class EnterDataActivity extends AppCompatActivity {
         mDoctorName = findViewById(R.id.et_doctor_name);
         mTranscript = findViewById(R.id.et_transcript);
         mAddButton = findViewById(R.id.bt_submit_data);
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new StoreConsultationTask().execute();
+            }
+        });
+    }
+
+    class StoreConsultationTask extends AsyncTask<Void,Void,Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+
+            String hospital = mHospitalName.getText().toString();
+            String doctor = mDoctorName.getText().toString();
+            String transcript = mTranscript.getText().toString();
+            /*
+            mHospitalName.setText("");
+            mDoctorName.setText("");
+            mTranscript.setText("");
+                */
+            Log.d("CONSULTATION","\n\n\nHospital: "
+                    + hospital + "\nDoctor: "
+                    + doctor + "\nTranscript:\n\""
+                    + transcript + "\"");
+
+            mHospitalName.getText().clear();
+            mDoctorName.getText().clear();
+            mTranscript.getText().clear();
+            // TODO store data in sqlite
+            return true;
+        }
     }
 }
